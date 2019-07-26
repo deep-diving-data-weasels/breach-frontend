2ec28df2da229320ead172b2e77086930fc4f9d1
Jack Daniel Kinne <bravelemming@gmail.com>
2019-07-25 18:03:17 -0700

    password validation, content added to aside and footer, picture, description.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,6 @@ export default class Footer extends Component {
       <Fragment>
         <footer>
         <a title='Realtime application protection' href='https://www.sqreen.com/?utm_source=badge'><img src='https://s3-eu-west-1.amazonaws.com/sqreen-assets/badges/20171107/sqreen-dark-badge.svg' alt='Sqreen | Runtime Application Protection' /></a>
-        <p>Our Promise: We use the middleware SQREEN for securing your data requests from this site.  We will not sell, release, or store results of your searches.</p>
         </footer>
       </Fragment>
     );
