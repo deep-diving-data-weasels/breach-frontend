858dd19c495a41f8979704ce430c272e7a48c8db
Jack Daniel Kinne <bravelemming@gmail.com>
2019-07-25 17:31:57 -0700

    added email regex validation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,10 @@ export default class Footer extends Component {
     console.log(this.state);
     return (
       <Fragment>
-        <footer>This is our footer.</footer>
+        <footer>
+        <a title='Realtime application protection' href='https://www.sqreen.com/?utm_source=badge'><img src='https://s3-eu-west-1.amazonaws.com/sqreen-assets/badges/20171107/sqreen-dark-badge.svg' alt='Sqreen | Runtime Application Protection' /></a>
+        <p>Our Promise: We use the middleware SQREEN for securing your data requests from this site.  We will not sell, release, or store results of your searches.</p>
+        </footer>
       </Fragment>
     );
   }
